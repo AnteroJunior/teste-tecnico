2c857e6fe4d0aab7b1d0f690ae103364e20ad604
Antero Junior <anterojunior1530@gmail.com>
2025-08-08 20:32:57 -0300

    fix: jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  testEnvironment: 'jsdom',
-  globalSetup: 'jest-preset-angular/global-setup',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  preset: "jest-preset-angular",
+  testEnvironment: "jsdom",
+  globalSetup: "jest-preset-angular/global-setup",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  moduleFileExtensions: ["ts", "html", "js", "json", "mjs"],
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    "^.+\\.(ts|mjs|js|html)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.(html)$",
+      },
+    ],
   },
-  resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver',
+  resolver: "jest-preset-angular/build/resolvers/ng-jest-resolver",
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-  testMatch: ['**/?(*.)+(spec).ts'],
-  transformIgnorePatterns: ['node_modules/(?!.*\\.(mjs|ts|js)$)'],
+  testMatch: ["**/?(*.)+(spec).ts"],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.(mjs|ts|js)$)"],
 };
-
-
